8d7bfe82bffcd7e7be448882913913517238a556
Siwawes Wongcharoen <siwaoh@gmail.com>
2016-01-06 21:01:06 +0700

    fixed bug

--- admin_upload_photo_process.php
+++ admin_upload_photo_process.php
@@ -26,6 +26,13 @@
       }
   }
 
+  // define variables and set to empty values
+  $product_id = "";
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $product_id = testInput($_POST["product_id"]);
+  }
+
   // Check if $uploadOk is set to 0 by an error
   if ($uploadOk == 0) {
       // echo "Sorry, your file was not uploaded.";
@@ -36,17 +43,10 @@
       if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $new_target_file)) {
           // echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
       } else {
-          echo "Sorry, there was an error uploading your file.";
-          header("Location: admin_product_view.php?id=$product_id&success=false&command=upload&reason=0");
+          // echo "Sorry, there was an error uploading your file.";
+          header("Location: admin_product_view.php?id=$product_id&success=false&command=upload&reason=1");
           die();
       }
-  }
-
-  // define variables and set to empty values
-  $product_id = "";
-
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $product_id = testInput($_POST["product_id"]);
   }
 
   $sql = "INSERT INTO product_photos (product_id, name, created_at, updated_at)
@@ -60,7 +60,7 @@
       // echo "Error: " . $sql . "<br>" . $conn->error;
     if ( strrpos($conn->error, "Duplicate") !== false ) {
       // echo "Duplicate";
-      header("Location: admin_product_view.php?id=$product_id&success=false&command=upload&reason=0");
+      header("Location: admin_product_view.php?id=$product_id&success=false&command=upload&reason=2");
       die();
     } else {
       echo $conn->error;
